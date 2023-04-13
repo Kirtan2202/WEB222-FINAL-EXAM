967d629dfcf1e65eca2294deb12766a3fc8079d5
Kirtan2202 <130610441+Kirtan2202@users.noreply.github.com>
2023-04-12 23:24:45 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+window.reviewData = [
+    {
+        name: "Usman",
+        date: "2023-03-13",
+        rating: 5,
+        review: "Best game to play when you are bored."
+    },
+    {
+        name: "Bhavri",
+        date: "2023-03-9",
+        rating: 5,
+        review: "Best Time pass game"
+    },
+    {
+        name: "Tanveer ",
+        date: "2023-03-09",
+        rating: 3,
+        review: "It's good but Need some improvement like fixing some bugs"
+    },
+    {
+        name: "Salman",
+        date: "2023-03-10",
+        rating: 5,
+        review: "Absolutely amazing! Addictive game"
+    },
+    {
+        name: "sweety",
+        date: "2023-03-12",
+        rating: 4,
+        review: "Bhai, Very good"
+    }
+];
